12461a186bb0fb11daba39677dc29d6d66b2f322
stijn <s.j.heldens@tudelft.nl>
2016-02-01 16:41:44 +0100

    Moved configuration settings from config.cmake to config/powergraph.properties

--- prepare-benchmark.sh
+++ prepare-benchmark.sh
@@ -18,8 +18,24 @@
 export platform="powergraph"
 
 # Build binaries
+if [ -z $POWERGRAPH_HOME ]; then
+    POWERGRAPH_HOME=`awk -F' *= *' '{ if ($1 == "powergraph.home") print $2 }' $config/powergraph.properties`
+fi
+
+if [ -z $POWERGRAPH_HOME ]; then
+    echo "Error: home directory for GraphMat not specified."
+    echo "Define the environment variable \$POWERGRAPH_HOME or modify powergraph.home in $config/powergraph.properties"
+    exit 1
+fi
+
+if [ -z $DISABLE_MPI ]; then
+    DISABLE_MPI=`awk -F' *= *' '{ if ($1 == "powergraph.disable_mpi") print $2 }' $config/powergraph.properties`
+fi
+
+DISABLE_JVM=1
+
 mkdir -p bin
-(cd bin && cmake -DCMAKE_BUILD_TYPE=Release ../src/ && make all)
+(cd bin && cmake -DCMAKE_BUILD_TYPE=Release ../src/ -DPOWERGRAPH_HOME=$POWERGRAPH_HOME -DNO_JVM=$DISABLE_JVM -DNO_MPI=$DISABLE_MPI && make all)
 
 if [ $? -ne 0 ]
 then
--- src/main/c/config.cmake
+++
@@ -1,8 +0,0 @@
-# Root directory where PowerGraph has been installed
-#set (POWERGRAPH_HOME ~/PowerGraph/)
-
-# Enable if PowerGraph has been compiled without JVM support (-no_jvm)
-set (NO_JVM 1)
-
-# Enable if PowerGraph has been compiled without MPI support (-no_mpi)
-set (NO_MPI 0)
