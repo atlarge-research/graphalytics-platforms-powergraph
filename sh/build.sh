b7dcf3b783534fdcd06aa1f1d1533fa96898cde6
Wing Lung Ngai <w.l.ngai@vu.nl>
2017-04-28 12:30:06 +0200

    Update build script for prebuilding distribution.

--- sh/build.sh
+++ sh/build.sh
@@ -7,7 +7,7 @@ module load openmpi/gcc/64/1.10.1
 module list
 
 # change Boost src to http://kent.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.gz in CMakeList.
-cd /var/scratch/wlngai/graphalytics/app/powergraph/src/
+cd /var/scratch/wlngai/graphalytics/large-platforms/powergraph/src/
 rm -rf deps/*
 ./configure --no_jvm
 cd release
@@ -24,7 +24,7 @@ module load openmpi/gcc/64/1.10.1
 module list
 
 
-POWERGRAPH_HOME=/var/scratch/wlngai/graphalytics/app/powergraph/src/
+POWERGRAPH_HOME=/var/scratch/wlngai/graphalytics/large-platforms/powergraph/src/
 
 mkdir -p bin/standard
 (cd bin/standard && cmake -DCMAKE_BUILD_TYPE=Release ../../src/main/c -DPOWERGRAPH_HOME=$POWERGRAPH_HOME -DNO_JVM=1 -DNO_MPI=0 && make all)
